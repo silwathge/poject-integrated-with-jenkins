b0a1308b85462ad2d82df4e35a9063e1f893b593
kapila_silwathge <kapila_silwathge@SLL013517.persistent.co.in>
2020-12-12 18:49:42 +0530

    sdfsdf

--- src/main/java/com/example/demo/JenkinsMavenApplication.java
+++ src/main/java/com/example/demo/JenkinsMavenApplication.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsMavenApplication {
+public class JenkinsMavenApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		System.out.println("Hi there edited, jenkins should pick this up now");
