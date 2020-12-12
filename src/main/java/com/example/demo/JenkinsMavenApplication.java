a9bdbaf51a77af5e83d4ababff3899cba2a63b9a
Kapila Silwathge <kapila_silwathge@persistent.co.in>
2020-12-12 14:46:54 +0530

    init

---
+++ src/main/java/com/example/demo/JenkinsMavenApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMavenApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hi there");
+		SpringApplication.run(JenkinsMavenApplication.class, args);
+	}
+
+}
